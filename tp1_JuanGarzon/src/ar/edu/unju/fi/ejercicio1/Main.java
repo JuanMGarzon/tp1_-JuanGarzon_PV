ad89088561c00cf1519307546b62b836c1ebcad0
JuanMGarzon <166052440+JuanMGarzon@users.noreply.github.com>
2024-04-04 17:49:06 -0300

    Add files via upload
    Ejercicio 1

---
+++ tp1_JuanGarzon/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 8, num2 = 5, num3 = 1, num4 = 20, num5 = 9;
+		
+		double promedio = (num1+num2+num3+num4+num5) / 5.0;
+		
+		System.out.println("El promedio es: " + promedio);
+		
+	}
+}
+
